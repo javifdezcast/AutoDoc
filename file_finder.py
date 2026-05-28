65614f51b33c7b950166adfd58b2f702c271a2f0
Javier Fernandez Castiella <jfdezcast@gmail.com>
2026-05-28 21:07:33 +0200

    Busca ficheros segun estrategia de documentación

--- file_finder.py
+++ file_finder.py
@@ -1,18 +1,24 @@
 import os
 from typing import Iterable
+from git import Repo
+from languages import languages
 
 
-class Git:
+class FileFinder:
 
     def __init__(self):
         pass
 
     @classmethod
     def find_different_files(cls,config: dict) -> list[str]:
-        return cls.find_matching_files(
-            config[config['language']]['root'],
-            config[config['language']]['extensions']
-        )
+        root = config['root_dir']
+        langauge_files = cls.find_matching_files(root, languages[config['language']]['extensions'])
+        files_to_document = langauge_files
+        if (config['mode'] == 'diff'):
+            different_files = cls.find_changed_files(root)
+            files_to_document = list(set(langauge_files) & set(different_files))
+        return files_to_document
+
 
 
     @classmethod
@@ -30,5 +36,23 @@ class Git:
         return matches
 
     @classmethod
-    def find_changed_files(cls, root: str, extensions: Iterable[str]) -> list[str]:
-        
+    def find_changed_files(cls, root: str) -> list[str]:
+        repo = Repo(root)
+        head_commit = repo.head.commit
+        if not head_commit.parents:
+            changed = {
+                blob.path
+                for blob in head_commit.tree.traverse()
+                if blob.type == 'blob'
+            }
+            return [os.path.join(root, p) for p in sorted(changed)]
+        parent_commit = head_commit.parents[0]
+        diff_index = parent_commit.diff(head_commit)
+        changed: set[str] = set()
+        for change_type in ('A', 'D', 'M', 'R'):
+            for diff in diff_index.iter_change_type(change_type):
+                if diff.a_path:
+                    changed.add(diff.a_path)
+                if diff.b_path:
+                    changed.add(diff.b_path)
+        return [os.path.join(root, p) for p in sorted(changed)]
