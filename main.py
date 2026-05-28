d5780114e012125c4e9697c70265d3f824a1e471
Javier Fernandez Castiella <jfdezcast@gmail.com>
2026-05-28 21:07:55 +0200

    Añade mensages de depuración

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 import json
 from pathlib import Path
 from factory import Factory
-from git import Git
+from file_finder import FileFinder
 
 
 def main():
     config = json.load(open('config.json'))
-    files_to_document = Git.find_different_files()
+    files_to_document = FileFinder.find_different_files(config)
     documenter = Factory.get_documenter(config['language'], config['llm']['model'])
-    for file in files_to_document:
+    for i, file in enumerate(files_to_document, start=1):
+        print(f"Documenting file {i}/{len(files_to_document)}: {file}")
         documenter.document_file(Path(file))
 
 # Press the green button in the gutter to run the script.
